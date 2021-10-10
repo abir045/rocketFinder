33b38999ff44d98e9ec2a21726281324634eca53
Salekin Imran <abir045@gmail.com>
2021-10-10 15:13:18 +0600

    removed search funcionalityh

--- src/components/RocketListing/RocketListing.js
+++ src/components/RocketListing/RocketListing.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { getAllRockets } from "../../features/rockets/rocketSlice";
 import RocketCard from "../RocketCard/RocketCard";
@@ -6,38 +6,24 @@ import "./RocketListing.css";
 
 const RocketListing = (props) => {
   const rockets = useSelector(getAllRockets);
-  const [searchTitle, setSearchTitle] = useState("");
-  const [output, setOutput] = useState([]);
-
-  useEffect(() => {
-    setOutput([]);
-    rockets.filter((val) => {
-      if (
-        val.rocket.rocket_name.toLowerCase().includes(searchTitle.toLowerCase())
-      ) {
-        setOutput((output) => [...output, val]);
-      }
-    });
-  }, [searchTitle, rockets]);
 
   return (
     <div className="rocket-wrapper">
       <div className="rocket-list">
         <h2>Rockets</h2>
-        <form className="d-flex">
+        <form class="d-flex">
           <input
             className="form-control me-2"
             type="search"
             placeholder="Search"
             aria-label="Search"
-            onChange={(e) => setSearchTitle(e.target.value)}
           />
           <button className="btn btn-outline-success" type="submit">
             Search
           </button>
         </form>
         <div className="row">
-          {output.map((rocket, index) => (
+          {rockets.map((rocket, index) => (
             <div className="col">
               <RocketCard key={index} data={rocket} />
             </div>
